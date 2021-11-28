3d08a7144f5ffa1893b65eab6ba858a03b0c2ee4
Adimov, Denis <CSTADIMOD@CORP.JTI.COM>
2021-11-28 18:17:31 +0200

    Add project files.

---
+++ Core/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace Core.Middlewares
+{
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next)
+            => _next = next;
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next.Invoke(context);
+            }
+            catch (Exception exception)
+            {
+                context.Response.Redirect("/home");
+            }
+        }
+    }
+}
---
+++ Core/Middlewares/LoggerMiddleware.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Core.Middlewares
+{
+    public class LoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ILogger Logger { get; }
+
+        public LoggerMiddleware(RequestDelegate next, ILogger logger)
+        {
+            _next = next;
+            Logger = logger;
+        }
+    }
+}
